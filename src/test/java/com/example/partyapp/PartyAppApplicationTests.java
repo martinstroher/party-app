0fcdceddfcdc8c60919a02f6a01f4a284343c95b
Martin Ströher <martin.andrade@townsq.com.br>
2023-08-05 13:16:37 -0300

    Base project.

---
+++ src/test/java/com/example/partyapp/PartyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
