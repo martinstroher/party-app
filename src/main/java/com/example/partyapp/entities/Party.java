c242a9f85b67ae7cf85070cf9684dbc04bd6be2d
Martin Ströher <martin.andrade@townsq.com.br>
2023-08-06 00:58:35 -0300

    Implemented raw crud operations using Spring Data Rest.

--- src/main/java/com/example/partyapp/entities/Party.java
+++ src/main/java/com/example/partyapp/entities/Party.java
@@ -1,11 +1,15 @@
 package com.example.partyapp.entities;
 
 import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Party")
 @Data
 public class Party {
+    @Id
+    private ObjectId id;
     private String title;
     private String address;
 }
--- src/main/java/com/example/partyapp/repositories/PartyRepository.java
+++ src/main/java/com/example/partyapp/repositories/PartyRepository.java
@@ -1,7 +1,10 @@
 package com.example.partyapp.repositories;
 
+import com.example.partyapp.entities.Party;
+import org.bson.types.ObjectId;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PartyRepository {
+public interface PartyRepository extends CrudRepository<Party, ObjectId> {
 }
